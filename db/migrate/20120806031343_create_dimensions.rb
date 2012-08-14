b72a4bad5d8a650fb3bb23903b8939ea6ceee486
Ilya Scharrenbroich <ilya@Ilyas-MacBook-Air.local>
2012-08-13 21:02:20 -0400

    removing unused table

--- db/migrate/20120806031343_create_dimensions.rb
+++
@@ -1,9 +0,0 @@
-class CreateDimensions < ActiveRecord::Migration
-  def change
-    create_table :dimensions do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120808033216) do
 
-  create_table "dimensions", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
     t.string   "username"
