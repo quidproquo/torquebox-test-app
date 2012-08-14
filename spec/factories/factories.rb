188512541f692381fbc054b3574b2de5cc3046eb
Ilya Scharrenbroich <ilya@Ilyas-MacBook-Air.local>
2012-08-14 12:26:37 -0400

    adding rspec tests for models

---
+++ spec/factories/factories.rb
@@ -0,0 +1,41 @@
+
+FactoryGirl.define do
+
+  factory :account do
+    
+  end
+
+  factory :product do
+    name { 'test' }
+    price { Random.rand(0.0..1.0) }
+  end
+
+  factory :order do
+    # Fields:
+    status { 'draft' }
+    side { 'buy' }
+    type { 'market' }
+    price { Random.rand(0.1..1.0) }
+    original_quantity { 100 }
+    pending_quantity { 100 }
+
+    # Associations:
+    product { create(:product) }
+    account { create(:account) }
+  end
+
+  factory :buy_market_order, class: Order do
+    # Fields:
+    status { 'draft' }
+    side { 'buy' }
+    type { 'market' }
+    price { Random.rand(0.1..1.0) }
+    original_quantity { 100 }
+    pending_quantity { 100 }
+
+    # Associations:
+    product { create(:product) }
+    account { create(:account) }
+  end
+
+end
