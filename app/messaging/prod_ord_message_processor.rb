3c8642c639f3a11665e6975bef08c7627e9775c8
Ilya Scharrenbroich <ilya@Ilyas-MacBook-Air.local>
2012-08-15 12:05:12 -0400

    refactoring message processor and rspec tests

--- app/messaging/prod_ord_message_processor.rb
+++
@@ -1,9 +0,0 @@
-class ProdOrdMessageProcessor < TorqueBox::Messaging::MessageProcessor
-
-  def on_message(body)
-    puts "Processing message: #{body}"
-    sleep(2)
-    puts "Processing message: #{body}!"
-  end
-
-end
