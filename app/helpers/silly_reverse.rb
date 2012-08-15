fd447df82cb04c379763251336ae9febfb90977d
Ilya Scharrenbroich <ilya@Ilyas-MacBook-Air.local>
2012-08-15 17:58:15 -0400

    adding test questions

---
+++ app/helpers/silly_reverse.rb
@@ -0,0 +1,19 @@
+module SillyReverse
+
+  def self.silly_reverse!(string)
+    string.chars.each_with_index { |c,i|
+      if i <= string.length/2
+        string[i], string[-(i+1)] = string[-(i+1)], string[i]
+      else
+        break
+      end
+    }
+  end
+
+  def silly_reverse!
+    SillyReverse.silly_reverse!(self)
+  end
+
+end
+
+
---
+++ spec/questions/silly_reverse_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+class String
+  include SillyReverse
+end
+
+describe SillyReverse do
+
+  it 'should reverse the string in place' do
+    a = 'Hello, World!'
+    SillyReverse.silly_reverse!(a)
+    a.should == '!dlroW ,olleH'
+  end
+
+  it 'should perform reverse on string' do
+    a = 'Hello, World!'
+    a.silly_reverse!
+    a.should == '!dlroW ,olleH'
+  end
+
+end
