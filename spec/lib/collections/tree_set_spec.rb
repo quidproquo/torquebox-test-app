c3a95d0516cfae3767105c5d48fb2b276aed38d2
Ilya Scharrenbroich <ilya@Ilyas-MacBook-Air.local>
2012-08-18 15:14:02 -0400

    adding more tests for tree_set

--- spec/lib/collections/tree_set_spec.rb
+++ spec/lib/collections/tree_set_spec.rb
@@ -3,7 +3,8 @@ require 'lib/collections/tree_set'
 
 
 describe TreeSet do
-  subject { TreeSet.new }
+  let(:comparator) { nil }
+  subject { TreeSet.new(&comparator) }
 
   describe :initialize do
 
@@ -12,7 +13,7 @@ describe TreeSet do
       its(:comparator) { should_not == nil }
     end
 
-    context 'with comparator' do
+    context 'with custom comparator' do
       let(:comparator) { Proc.new { |a,b| a <=> b } }
       subject { TreeSet.new(&comparator) }
       its(:comparator) { should == comparator }
@@ -24,31 +25,47 @@ describe TreeSet do
     
     describe :add do
       let(:items) { [] }
-      let(:items_expected) { items.uniq!; items.sort! }
+      let(:items_expected) { [] }
       before do
         items.each { |item|
           subject << item
         }
       end
 
-      context 'when items are all unique in order' do
-        let(:items) { [1, 2, 3, 4, 5] }
-        it { should == items_expected }
-      end
+      context 'with natural comparator' do
+        let(:items_expected) { items.uniq!; items.sort! }
 
-      context 'when items are all unique and not in order' do
-        let(:items) { [1, 3, 5, 2, 4] }
-        it { should == items_expected }
-      end
+        context 'when items are all unique in order' do
+          let(:items) { [1, 2, 3, 4, 5] }
+          it { should == items_expected }
+        end
 
-      context 'when items are not unique and in order' do
-        let(:items) { [1, 1, 1, 2, 2, 3, 4, 5, 5, 5, 5] }
-        it { should == items_expected }
-      end
+        context 'when items are all unique and not in order' do
+          let(:items) { [1, 3, 5, 2, 4] }
+          it { should == items_expected }
+        end
 
-      context 'when items are not unique and not in order' do
-        let(:items) { [1, 1, 1, 3, 3, 5, 2, 2, 2, 2, 4, 4] }
-        it { should == items_expected }
+        context 'when items are not unique and in order' do
+          let(:items) { [1, 1, 1, 2, 2, 3, 4, 5, 5, 5, 5] }
+          it { should == items_expected }
+        end
+
+        context 'when items are not unique and not in order' do
+          let(:items) { [1, 1, 1, 3, 3, 5, 2, 2, 2, 2, 4, 4] }
+          it { should == items_expected }
+        end
+
+      end # natural comparator
+
+      context 'with custom comparator' do
+        let(:comparator) { Proc.new { |a,b| a.size <=> b.size } }
+
+        context 'with nonunique items not in order' do
+          let(:items) { ['this', 'this', 'was', 'a', 'funny', 'pepper'] }
+          let(:items_expected) { ['a', 'was', 'this', 'funny', 'pepper'] }
+          it { should == items_expected }
+        end
+
       end
 
     end # add
